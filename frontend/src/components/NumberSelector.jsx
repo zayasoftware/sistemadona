6d57ea06c89d1cf04d1bd727c28ec6593efd1327
JMathRA <j.matheusalbuquerque1@gmail.com>
2025-01-23 19:28:32 -0300

    alterações no layout

--- frontend/src/components/NumberSelector.jsx
+++ frontend/src/components/NumberSelector.jsx
@@ -1,8 +1,22 @@
 import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types'; // Importe a biblioteca prop-types
+import PropTypes from 'prop-types';
 
 const NumberSelector = ({ onSelect }) => {
   const [selectedNumbers, setSelectedNumbers] = useState([]);
+
+  // Lista de cores para os números
+  const colors = [
+    'bg-red-500',
+    'bg-blue-500',
+    'bg-yellow-500',
+    'bg-purple-500',
+    'bg-pink-500',
+    'bg-indigo-500',
+    'bg-green-500',
+    'bg-orange-500',
+    'bg-teal-500',
+    'bg-gray-500',
+  ];
 
   const handleNumberClick = (number) => {
     if (selectedNumbers.includes(number)) {
@@ -18,14 +32,16 @@ const NumberSelector = ({ onSelect }) => {
   }, [selectedNumbers, onSelect]);
 
   return (
-    <div className="flex justify-center my-4">
-      <div className="grid grid-cols-6 gap-2">
+    <div className="flex justify-center my-10">
+      <div className="grid grid-cols-6 gap-2 rounded-lg">
         {Array.from({ length: 30 }, (_, i) => i + 1).map(number => (
           <div
             key={number}
             onClick={() => handleNumberClick(number)}
-            className={`w-12 h-12 flex items-center justify-center text-xl font-bold rounded-lg cursor-pointer ${
-              selectedNumbers.includes(number) ? 'bg-blue-500 text-white' : 'bg-gray-300'
+            className={`w-16 h-16 flex items-center justify-center text-xl font-bold rounded-full cursor-pointer ${
+              selectedNumbers.includes(number)
+                ? 'bg-white-500 text-white' // Cor quando o número está selecionado
+                : colors[number % colors.length] // Cor de fundo com base no número
             }`}
           >
             {number}
@@ -37,7 +53,7 @@ const NumberSelector = ({ onSelect }) => {
 };
 
 NumberSelector.propTypes = {
-  onSelect: PropTypes.func.isRequired, 
+  onSelect: PropTypes.func.isRequired,
 };
 
 export default NumberSelector;
